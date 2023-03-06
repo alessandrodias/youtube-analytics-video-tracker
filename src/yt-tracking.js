287637fdd5e53ff6f52fdf6556dd65a7b9ef0d5a
Alessandro Dias <ale.dias@hotmail.com.br>
2023-03-06 09:18:13 -0300

    class constructor for YouTube Video Tracker

--- src/yt-tracking.js
+++ src/yt-tracking.js
@@ -1,113 +1,100 @@
-/*
-  Author: Alessandro Dias
-    Demo: http://alessandrodias.github.io/youtube-tracker
-    Repo: https://github.com/alessandrodias/youtube-tracker
- */
+class YtTracking {
+  /**
+   * YtTracking class
+   * @param {object} player - YouTube player object
+   * @param {object} options
+   * @returns
+   */
+  constructor(player, options) {
+    if ((player === null) || (player === undefined)) {
+      throw new Error('Missing YouTube player object');
+      return;
+    }
 
-;(function(window, document, undefined) {
-	"use strict";
+    if ((!options.callback) || (options.callback === null) || (typeof options.callback !== 'function')) {
+      throw new Error('Missing the callback function');
+      return;
+    }
 
-	/**	----- YouTube Video States -----
-	* 	 [-1] Not Started
-	* 	 [0]  Ended
-	* 	 [1]  Playing
-	* 	 [2]  Paused
-	*/
+    this.player = player;
+    this.trackValues = options.trackings || ['0%', '25%', '50%', '75%', '100%'];
+    this.callback = options.callback;
+    this.trackList = [];
+    this.videoTimer = null;
 
-	function YtTracking(player, options) {
-		if ((player === null) || (player === undefined)) {
-			throw new Error('Missing player object');
-			return;
-		}
+    this.setupTrackList();
+  }
 
-		if ((options.callback === null) || (typeof options.callback !== 'function')) {
-			throw new Error('Missing the callback function');
-			return;
-		}
+  setupTrackList = () => {
+    for (var i = 0, j = this.trackValues.length; i < j; i++) {
+      this.trackList[this.trackValues[i]] = false;
+    }
+  }
 
-		this.player = player;
-		this.trackValues = options.trackings || ['0%', '25%', '50%', '75%', '100%'];
-		this.callback = options.callback;
-		this.trackList = [];
-		this.videoTimer = null;
+  onVideoStateChange = (state) => {
+    // YouTube Video States
+    // -1 = Not Started
+    //  0 = Ended
+    //  1 = Playing
+    //  2 = Paused
 
-		this.setupTrackList();
-	}
+    switch(state) {
+      case YT.PlayerState.PLAYING:
+        this.startVideoTimer();
+        break;
+      case YT.PlayerState.ENDED:
+        this.verifyVideoTime();
+        this.stopVideoTimer();
+        break;
+      default:
+        this.stopVideoTimer();
+        break;
+    }
+  }
 
-	YtTracking.prototype.setupTrackList = function() {
-		for(var i = 0, j = this.trackValues.length; i < j; i++) {
-			this.trackList[this.trackValues[i]] = false;
-		}
-	};
+  startVideoTimer = () => {
+    this.videoTimer = setInterval(() => {
+      this.verifyVideoTime();
+    }, 1000);
+  }
 
-	YtTracking.prototype.onVideoStateChange = function(state) {
-		switch(state) {
-			case YT.PlayerState.PLAYING:
-				this.startVideoTimer();
-				break;
-			case YT.PlayerState.ENDED:
-				this.verifyVideoTime();
-				this.stopVideoTimer();
-				break;
-			default:
-				this.stopVideoTimer();
-				break;
-		}
-	};
+  stopVideoTimer = () => {
+    clearInterval(this.videoTimer);
+  }
 
-	YtTracking.prototype.startVideoTimer = function() {
-		var _self = this;
-		this.videoTimer = setInterval( function() {
-			_self.verifyVideoTime();
-		}, 1000);
-	};
+  verifyVideoTime = () => {
+    const totalTime = this.player.getDuration();
+    const elapsedTime = Math.round(this.player.getCurrentTime());
+    let type, currentValue, percentage = 0;
 
-	YtTracking.prototype.stopVideoTimer = function() {
-		clearInterval(this.videoTimer);
-	};
+    for(var i = 0, j = this.trackValues.length; i < j; i++) {
+      type = this.trackValues[i].substr(this.trackValues[i].length -1);
+      currentValue = this.trackValues[i].replace(type, '');
 
-	YtTracking.prototype.verifyVideoTime = function() {
-		var totalTime, elapsedTime, type, currentValue, percentage = 0;
+      if (!this.trackList[this.trackValues[i]]) {
+        if (type === '%') {
+          // track by percentage
+          percentage = Math.round(((100 * elapsedTime) / totalTime));
 
-		totalTime = this.player.getDuration();
-		elapsedTime = Math.round( this.player.getCurrentTime() );
+          if (percentage >= currentValue) {
+            this.refreshTrackList(this.trackValues[i]);
+          }
+        } else if (type === 's') {
+          // track by seconds
+          if (elapsedTime >= currentValue) {
+            this.refreshTrackList(this.trackValues[i]);
+          }
+        }
+      }
+    }
+  }
 
-		for(var i = 0, j = this.trackValues.length; i < j; i++) {
+  refreshTrackList = (trackValue) => {
+    this.trackList[trackValue] = true;
+    this.publish(trackValue);
+  }
 
-			type = this.trackValues[i].substr( this.trackValues[i].length -1 );
-			currentValue = this.trackValues[i].replace(/[a-z]/g,"");
-
-			if (type === 'p') {
-
-				// tracking by percentage
-				percentage = Math.round( ((100 * elapsedTime) / totalTime) );
-
-				if ( (percentage >= currentValue) && (this.trackList[this.trackValues[i]] === false) ) {
-					this.refreshTrackList(this.trackValues[i]);
-				}
-
-			} else if (type === 's') {
-
-				// tracking by seconds
-				if ( (elapsedTime >= currentValue) && (this.trackList[this.trackValues[i]] === false) ) {
-					this.refreshTrackList(this.trackValues[i]);
-				}
-
-			}
-		}
-	};
-
-	YtTracking.prototype.refreshTrackList = function(track_value) {
-		this.trackList[track_value] = true;
-		this.publish(track_value);
-	}
-
-	YtTracking.prototype.publish = function(trackValue) {
-		if ( (this.callback) && (typeof this.callback === 'function') ) {
-			this.callback(trackValue);
-		}
-	};
-
-	window.YtTracking = YtTracking;
-
-})(this, this.document);
+  publish = (trackValue) => {
+    this.callback(trackValue);
+  }
+}
